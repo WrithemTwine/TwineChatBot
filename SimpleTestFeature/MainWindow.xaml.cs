97a8b330048f9971e9d996c7b4dff38c97a6ee44
WrithemTwine <writhemdevelop@gmail.com>
2022-09-07 00:47:33 -0400

    created SimpleTestFeature project to simple test features before fully implementing in primary code

---
+++ SimpleTestFeature/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace SimpleTestFeature
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _ = typeof(MainWindow).InvokeMember(
+                "UpdateGUI",
+                System.Reflection.BindingFlags.InvokeMethod,
+                null, 
+                this, 
+                new[] { "Invoke Member worked." });
+
+        }
+
+        public void UpdateGUI(string Text)
+        {
+            TextBlock_GUI_Text.Text = Text;
+        }
+    }
+}
