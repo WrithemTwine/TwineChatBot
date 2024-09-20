4a57ea406c3d45afab4ece724251e6c23836d4c5
WrithemTwine <writhemdevelop@gmail.com>
2024-09-20 00:59:21 -0400

    added a 'category' converter, needs more work to connect wkth the GUI-DataGrids

--- StreamerBotLib/DataSQL/Models/0TypeConverters.cs
+++ StreamerBotLib/DataSQL/Models/0TypeConverters.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace StreamerBotLib.DataSQL.Models
@@ -95,6 +96,32 @@ namespace StreamerBotLib.DataSQL.Models
             else
             {
                 return 0;
+            }
+        }
+    }
+
+    public class CategoryConverter : ValidationRule, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return  (List<string>)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Join(", ",((List<string>)value));
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            List<string> Data = value as List<string>;
+
+            if (Data.Count > 1 && Data.Contains("All"))
+            {
+                return new(false, "All or other categories, not both.");
+            } else
+            {
+                return new(true, "Successful.");
             }
         }
     }
